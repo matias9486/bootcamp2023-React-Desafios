6c4182aec267fe9ab88f264877021dbb510cde34
matias9486 <matias9486mdp@gmail.com>
2024-01-02 21:10:02 -0300

    Fix: se cambio el origen de datos a un archivo externo y se importo el mismo en el componente principal

--- desafio1/src/cv/MiCVComponent.jsx
+++ desafio1/src/cv/MiCVComponent.jsx
@@ -4,56 +4,9 @@ import { DatosPersonalesComponent } from './DatosPersonalesComponent.jsx';
 import { ExperienciaLaboralComponent } from './ExperienciaLaboralComponent.jsx';
 import { SobreMiComponent } from './SobreMiComponent.jsx';
 import { FotoComponent } from './FotoComponent.jsx';
+import {datosPersonales, sobreMi, educacion, experienciaLaboral} from '../utils/data.js';
 
 export const MiCVComponent = () => {    
-    const datosPersonales = {
-        email:"matias9486mdp@gmail.com ",
-        celular:"+542235943972 ",
-        github:"https://github.com/matias9486",
-        portfolio:"https://matias-portfolio.netlify.app/",
-        linkedin:"https://www.linkedin.com/in/matias-alancay/",
-        lugar:"Mar del Plata, Buenos Aires"
-    }
-
-    const sobreMi = 'Estudiante de tecnología con ganas de empezar a trabajar como desarrollador en una empresa donde pueda aprender y crecer profesionalmente.';
-    
-    const educacion = {                
-        establecimiento:'UTN San Rafael, Mendoza',
-        titulo: 'Tecnicatura Universitaria en Programacion. Finalizando 2º Año (último)',
-        anio: '(2022 - 2023)'        
-    }
-    
-    const experienciaLaboral = 
-        [
-            {
-                id:4,
-                lugar: null,
-                puesto: "Servicio Técnico Particular",
-                fecha:"2015 - Hasta la fecha",
-                tareas: ["Diagnóstico de problemas y resolución", "Instalación, mantenimiento y actualización de Software y Hardware"]
-            },
-            {
-                id:3,
-                lugar:"Universidad FASTA",
-                puesto: "Soporte Técnico ",
-                fecha:"2014 - 2015",
-                tareas: ["Asistencia a Docentes y Personal de la Universidad" ,"Diagnóstico de problemas y resolución" ,"Instalación, mantenimiento y actualización de Software y Hardware", "Trabajo en equipo para realizar tareas"]
-            },
-            {
-                id:2,
-                lugar:"Universidad FASTA",
-                puesto: "Auxiliar de Laboratorios",
-                fecha:"2011 - 2015",
-                tareas: ["Mantenimiento y control de Laboratorios de Informática", "Asistencia a Docentes y Alumnos", "Diagnóstico de problemas y resolución", "Instalación, mantenimiento y actualización de Software y Hardware", "Trabajo en equipo para realizar tareas"]
-            },
-            {
-                id:1,
-                lugar:"Universidad FASTA",
-                puesto: "Voluntario (Dpto Informática)",
-                fecha:"2008 - 2011",
-                tareas: ["Mantenimiento y control de Laboratorios de Informática", "Asistencia a Docentes y Alumnos", "Diagnóstico de problemas y resolución", "Instalación, mantenimiento y actualización de Software y Hardware", "Trabajo en equipo para realizar tareas"]
-            }
-        ];
     
     return (
         <>
---
+++ desafio1/src/utils/data.js
@@ -0,0 +1,48 @@
+export const datosPersonales = {
+    email:"matias9486mdp@gmail.com ",
+    celular:"+542235943972 ",
+    github:"https://github.com/matias9486",
+    portfolio:"https://matias-portfolio.netlify.app/",
+    linkedin:"https://www.linkedin.com/in/matias-alancay/",
+    lugar:"Mar del Plata, Buenos Aires"
+}
+
+export const sobreMi = 'Estudiante de tecnología con ganas de empezar a trabajar como desarrollador en una empresa donde pueda aprender y crecer profesionalmente.';
+
+export const educacion = {                
+    establecimiento:'UTN San Rafael, Mendoza',
+    titulo: 'Tecnicatura Universitaria en Programacion. Finalizando 2º Año (último)',
+    anio: '(2022 - 2023)'        
+}
+
+export const experienciaLaboral = 
+    [
+        {
+            id:4,
+            lugar: null,
+            puesto: "Servicio Técnico Particular",
+            fecha:"2015 - Hasta la fecha",
+            tareas: ["Diagnóstico de problemas y resolución", "Instalación, mantenimiento y actualización de Software y Hardware"]
+        },
+        {
+            id:3,
+            lugar:"Universidad FASTA",
+            puesto: "Soporte Técnico ",
+            fecha:"2014 - 2015",
+            tareas: ["Asistencia a Docentes y Personal de la Universidad" ,"Diagnóstico de problemas y resolución" ,"Instalación, mantenimiento y actualización de Software y Hardware", "Trabajo en equipo para realizar tareas"]
+        },
+        {
+            id:2,
+            lugar:"Universidad FASTA",
+            puesto: "Auxiliar de Laboratorios",
+            fecha:"2011 - 2015",
+            tareas: ["Mantenimiento y control de Laboratorios de Informática", "Asistencia a Docentes y Alumnos", "Diagnóstico de problemas y resolución", "Instalación, mantenimiento y actualización de Software y Hardware", "Trabajo en equipo para realizar tareas"]
+        },
+        {
+            id:1,
+            lugar:"Universidad FASTA",
+            puesto: "Voluntario (Dpto Informática)",
+            fecha:"2008 - 2011",
+            tareas: ["Mantenimiento y control de Laboratorios de Informática", "Asistencia a Docentes y Alumnos", "Diagnóstico de problemas y resolución", "Instalación, mantenimiento y actualización de Software y Hardware", "Trabajo en equipo para realizar tareas"]
+        }
+    ];
